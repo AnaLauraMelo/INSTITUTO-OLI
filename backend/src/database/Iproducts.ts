07b0e6ba369ed42ed4fa1deaa9f1749726545c6a
AnaLauraMelo <111325841+AnaLauraMelo@users.noreply.github.com>
2023-02-21 17:36:44 -0300

    Delete Iproducts.ts

--- backend/src/database/Iproducts.ts
+++
@@ -1,7 +0,0 @@
-import { IProduct } from "../interface/products";
-
-const libro: IProduct = { title: "Actividades", description: "Actividades para hacer en casa", price: 500, code: 1, stock: 5 };
-const curso: IProduct = { title: "curso", description: "Curso conversacional", price: 2000, code: 2, stock: 10 };
-
-export { libro, curso }
-
